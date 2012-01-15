4f75b3a37fbb1f2a2a94666be43f5bab9030fd73
Vincent Sposato <vsposato@debian.sposatoenterprises.com>
2012-01-15 14:36:08 -0500

    Add live-build configs for tracking purposes, and updated iso and img files

---
+++ Disk Wipe Verifier/live-build/dwp-livecd/install_php_packages.sh
@@ -0,0 +1,2 @@
+# Install PHP and supporting packages
+sudo apt-get install php5-cli php-pear hdparm dmidecode sudo git
---
+++ Disk Wipe Verifier/live-build/dwp-liveusb/install_pear_packages.sh
@@ -0,0 +1,12 @@
+# Install Pear & PHP Command Line
+sudo apt-get install php-pear php5-cli
+
+# Update the pear primary channel
+sudo pear channel-update pear.php.net
+
+# Install XML_RPC
+sudo pear install -f XML_RPC
+
+# Install XML_RPC2
+sudo pear install -f XML_RPC2
+
